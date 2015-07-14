7e1f5c1ab823eda301a9c508700b1a3e5de59d59
Stef Janssen <steffos.janssen@gmail.com>
2015-07-14 15:20:20 +0200

    reading images, first try

--- initial_tests/first_caffe_test.py
+++ initial_tests/first_caffe_test.py
@@ -1,3 +1,27 @@
 __author__ = 'stefjanssen'
 
 import caffe
+import numpy as np
+data_path = "../../Nus-wide/1k"
+
+image = caffe.io.load_image("")
+
+image_width = 240
+image_height = 218
+
+def load_images():
+    train_file = open(data_path + "train.txt", 'r')
+    all_images = np.asarray([])
+    all_labels = np.asarray([])
+    for line in file:
+        temp_arr = line.split(" ")
+        image = caffe.io.load_image(data_path + temp_arr[0])
+        all_images = np.concatenate((all_images, image), axis=0)
+        labels = [i for l in temp_arr[1:] where i = int(l)]
+        all_labels.concatenate((all_labels, labels), axis=0)
+
+    print "shape of data: "
+    print np.shape(all_images)
+    print "shape of labels: "
+    print np.shape(all_labels)
+
